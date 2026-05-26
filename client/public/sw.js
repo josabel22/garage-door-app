367f5415f583177626aa6c7f6c7ab16f88704889
josabel22 <104046833+josabel22@users.noreply.github.com>
2026-05-25 22:07:32 -0600

    Add files via upload

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mg-portones-v12';
+const CACHE_NAME = 'mg-portones-v14';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './mg-logo.jpg', './supabase-config.js'];
 
 self.addEventListener('install', (event) => {
